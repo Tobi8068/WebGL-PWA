cf66db65b40e52ffeb29abc950034ea067186196
tobi8068 <hana.dev22@gmail.com>
2024-04-30 07:35:03 +1000

    updated

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -10,15 +10,11 @@ const contentToCache = [
 
 self.addEventListener('install', function (e) {
   console.log('[Service Worker] Install');
-
-  if (!e.request.url.startsWith('https')) {
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
-  }
-
+  e.waitUntil((async function () {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
 });
 
 self.addEventListener('fetch', function (e) {

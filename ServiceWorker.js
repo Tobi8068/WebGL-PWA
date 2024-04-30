c4c505d2b83e497ab41eacbc4fdb112f675f055b
tobi8068 <hana.dev22@gmail.com>
2024-04-30 16:58:32 +1000

    updated

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,18 +1,19 @@
 const cacheName = "Xex-PAWTestTemplate-1.0";
 const contentToCache = [
-  "/",
-  "index.html",
   "Build/TestWebGL.loader.js",
   "Build/TestWebGL.framework.js",
   "Build/TestWebGL.data",
   "Build/TestWebGL.wasm",
   "TemplateData/style.css",
-  "lib/thirdweb-unity-bridge.js",
-  "manifest.json",
+  'TemplateData/favicon.ico',
+  'TemplateData/icons/unity-logo-dark.png',
+  'TemplateData/icons/512.png',
+
 ];
 
 self.addEventListener('install', function (e) {
   console.log('[Service Worker] Install');
+
   e.waitUntil((async function () {
     const cache = await caches.open(cacheName);
     console.log('[Service Worker] Caching all: app shell and content');
@@ -26,17 +27,10 @@ self.addEventListener('fetch', function (e) {
     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
     if (response) { return response; }
 
-    // Check if the requested resource is part of the contentToCache array
-    if (contentToCache.includes(new URL(e.request.url, self.location).pathname.slice(1))) {
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      if (e.request.url.startsWith('http')) {
-        cache.put(e.request, response.clone());
-      }
-      return response;
-    } else {
-      return fetch(e.request);
-    }
+    response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
   })());
 });
